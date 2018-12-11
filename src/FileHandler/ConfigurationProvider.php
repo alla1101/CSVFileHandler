614e182cbbb9930d5adc4e0caabb7e64f0f71517
alla barakat <a2la1101@gmail.com>
2018-12-11 21:38:22 +0200

    not using laravel in validation no more wooow

--- src/FileHandler/ConfigurationProvider.php
+++ src/FileHandler/ConfigurationProvider.php
@@ -4,8 +4,10 @@ namespace a2la1101\csvhandler\FileHandler;
 
 use a2la1101\csvhandler\contracts\FileConfigurationProvider;
 
-use Illuminate\Validation\Validator;
-use Illuminate\Container\Container;
+use Illuminate\Validation\Factory;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Translation\FileLoader;
+use Illuminate\Translation\Translator;
 
 use a2la1101\csvhandler\Exceptions\ConfigurationsException;
 
@@ -20,6 +22,28 @@ class ConfigurationProvider implements FileConfigurationProvider{
 		"Format"=>"required|min:1",
 		"Permission"=>"required|regex:/^[(r)(w)]$/"
 	];
+
+	protected function getValidator(){
+		
+		// Create a Filesystem instance
+		$filesystem = new Filesystem();
+
+		// Create a new FileLoader instance specifying the translation path
+		$loader = new FileLoader($filesystem, dirname(dirname(__FILE__)) . '/lang');
+
+		// Specify the translation namespace
+		$loader->addNamespace('lang', dirname(dirname(__FILE__)) . '/lang');
+
+		// This is used to create the path to your validation.php file
+		$loader->load($lang = 'en', $group = 'validation', $namespace = 'lang');
+
+		$factory = new Translator($loader, 'en');
+
+		$validator=new Factory($factory);
+
+		return $validator;
+	
+	}
 
 	public function setConfigurationsDirectory($directory){
 		
@@ -60,7 +84,7 @@ class ConfigurationProvider implements FileConfigurationProvider{
 		
 		}
 
-		$validator=app("validator");
+		$validator=$this->getValidator();//app("validator");
 		
 		// Validation for the configurations are made here
 		
