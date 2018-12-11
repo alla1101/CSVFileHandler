f388e5d05335b9c8532603866663559725c41dcc
alla barakat <a2la1101@gmail.com>
2018-12-11 22:20:32 +0200

    updating config file

--- src/config/CSVHandler.php
+++ src/config/CSVHandler.php
@@ -7,6 +7,13 @@ return [
 		"Format"=>"x,y,z",
 		"Delimiter"=>",",
 		"Permission"=>"r"
-	]
+	],
+
+	"WRITETEST"=>[
+		"Directory"=>"/var/www/app/files/file_copy.csv",
+		"Format"=>"x;y;z",
+		"Delimiter"=>";",
+		"Permission"=>"w"
+	],
 
 ];
